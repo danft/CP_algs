8fe0e55486a20c4675c9b86633eeec9b7e52f04f
danft <danft@openmailbox.org>
2015-08-19 01:03:47 -0300

    Update extended_euclidean.cpp
    Added CRT explanation

--- math/extended_euclidean.cpp
+++ math/extended_euclidean.cpp
@@ -3,7 +3,13 @@ using namespace std;
 typedef long long ll;
 
 typedef pair<ll,ll> ii;
-
+/*
+Chinese Remainder Theorem:
+let N=n0.n1.n2....nm /*ni is the ith moduli, which are all pairwise coprime*/
+for each i, let yi=N/ni
+for each i, let zi be the inverse of yi mod ni (could be done with gcdEx)
+x = sum ai.yi.zi i from 0 to m
+*/
 ii extended_euclidean(ll a, ll b, ll &g) {
 	ll y=0, x=1;
 	ll yy=1, xx=0;
