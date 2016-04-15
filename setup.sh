06a533426ce5d1b19a6bcee5f478fc18f97bd349
danft <danft@openmailbox.org>
2016-04-15 11:06:23 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+if ! [ -a "$HOME/.vimrc" ]
+then
+	wget tiny.cc/danft2 -O "$HOME/.vimrc" &> /dev/null
+fi
+if ! [ -a "$HOME/te.cpp" ]
+then
+	wget tiny.cc/danft2 -O "$HOME/te.cpp" &> /dev/null
+fi
+
+setxkbmap -layout "us"
+
+echo "export CXXFLAGS=-std=c++0x" >> "$HOME/.bashrc"
