2a959d5758426e3fc077394682bbbd2afe9ded05
danft <danft@openmailbox.org>
2018-05-29 14:26:38 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define mk make_pair
+#define pb push_back
+#define For(a,b) for(int (i)=0; (i)<(b); (i++))
+#define Fo(a,b,c) for(int (i)=(b); (i)<=(c); (i++))
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+const int INF=0x3f3f3f3f;
+
+int main(void) {
+
+	return 0;
+}
